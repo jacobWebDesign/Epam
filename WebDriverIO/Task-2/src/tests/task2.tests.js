620aa80008d55850bcfae9467e3641d077124d0f
Jakub Podgórski <jakkub522@gmail.com>
2024-05-08 11:07:51 +0200

    Fixing typo

--- WebDriverIO/Task-2/src/tests/task2.tests.js
+++ WebDriverIO/Task-2/src/tests/task2.tests.js
@@ -59,7 +59,7 @@ describe('Task 2', () => {
 
     it("Checks the title of the paste", async () => {
         await resultPage.title.waitForDisplayed();
-        await expect(resultPage.title).toHaveText('how to gain dominance among developer');
+        await expect(resultPage.title).toHaveText('how to gain dominance among developers');
     })
 
     it("Verifies if the text is suspended for bash", async() => {
